f132a13e47294e2452132ad3ff98f15911da4cec
LumedK <83239301+LumedK@users.noreply.github.com>
2022-04-02 14:45:35 +0500

    test deploy

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true
-    // basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-    // assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
+    reactStrictMode: true,
+    images: {
+        loader: 'akamai',
+        path: ''
+    },
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
 }
 
 module.exports = nextConfig
